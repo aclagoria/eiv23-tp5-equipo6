f28f28539b1ce13e96c4df228049935d2b3a41c9
aclagoria <lagoriacarolina@gmail.com>
2023-07-06 12:40:37 -0300

    Prueba

--- src/main.c
+++ src/main.c
@@ -2,14 +2,21 @@
 
 int main(void){
     uint8_t buffer;
+    int ang1=90;
+
     SP_ComSerie_init();
     SP_Timer_init();
     SP_Timer_PWM();
     for (;;)
     {
         // recibir y transmitir lo recibido
-        SP_ComSerie_write(buffer);
-        SP_ComSerie_read(&buffer);
+        //SP_ComSerie_write(buffer);
+        //SP_ComSerie_read(&buffer);
+        //for(int j=0;j<60000;++j){ //Retardo por fuerza bruta
+        //    for(int volatile i=0;i<(8000000/(1000*13));++i); //Calibro para que sea de 1 milisegundo, Calificar como volatile para que el compilador no lo elimine
+        //}
+        SP_Timer_setPWM(ang1);
+        
     }
     return 0;
     
